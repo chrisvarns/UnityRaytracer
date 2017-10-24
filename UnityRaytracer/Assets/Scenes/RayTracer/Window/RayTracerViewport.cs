76e4607c21cbb61c5f61aab66b242b3dc8f8b8da
Chris Varnsverry <chris.varnsverry@virtualarts.co.uk>
2017-10-25 00:11:35 +0100

    White texture gets rendered to viewport

---
+++ UnityRaytracer/Assets/Scenes/RayTracer/Window/RayTracerViewport.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class RayTracerViewport : EditorWindow
+{
+    string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    static Texture windowImage;
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Raytracer/Viewport")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        RayTracerViewport window = EditorWindow.GetWindow(typeof(RayTracerViewport)) as RayTracerViewport;
+        window.Show();
+        windowImage = EditorGUIUtility.whiteTexture;
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 100, 100), windowImage);
+    }
+}
