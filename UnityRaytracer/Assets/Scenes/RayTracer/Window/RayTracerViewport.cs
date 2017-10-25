882d73488cba5027f138176a08b6e0d49c8db4db
Chris Varnsverry <chris.varnsverry@virtualarts.co.uk>
2017-10-25 21:13:18 +0100

    Updating a texture with some colors.

--- UnityRaytracer/Assets/Scenes/RayTracer/Window/RayTracerViewport.cs
+++ UnityRaytracer/Assets/Scenes/RayTracer/Window/RayTracerViewport.cs
@@ -2,10 +2,9 @@
 using UnityEditor;
 public class RayTracerViewport : EditorWindow
 {
-    string myString = "Hello World";
-    bool groupEnabled;
-    bool myBool = true;
-    float myFloat = 1.23f;
+    static Texture2D texture;
+
+    static Color[] colors;
 
     // Add menu named "My Window" to the Window menu
     [MenuItem("Raytracer/Viewport")]
@@ -14,18 +13,22 @@ public class RayTracerViewport : EditorWindow
         // Get existing open window or if none, make a new one:
         RayTracerViewport window = EditorWindow.GetWindow(typeof(RayTracerViewport)) as RayTracerViewport;
         window.Show();
+        texture = new Texture2D(800, 600);
+        colors = new Color[800 * 600];
+        for (int y = 0; y < 600; y++)
+        {
+            for (int x = 0; x < 800; x++)
+            {
+                colors[(y * 800) + x] = new Color(((float)x % 256) / 256, ((float)y % 256) / 256, 0, 1);
+            }
+        }
     }
 
     void OnGUI()
     {
-        var cam = Camera.main;
-        RenderTexture currentRT = RenderTexture.active;
-        RenderTexture.active = cam.targetTexture;
-        cam.Render();
-        Texture2D image = new Texture2D(cam.targetTexture.width, cam.targetTexture.height);
-        image.ReadPixels(new Rect(0, 0, cam.targetTexture.width, cam.targetTexture.height), 0, 0);
-        image.Apply();
-        RenderTexture.active = currentRT;
-        GUI.DrawTexture(new Rect(0, 0, cam.targetTexture.width, cam.targetTexture.width), image);
+        if (texture == null) Init();
+        texture.SetPixels(colors);
+        texture.Apply();
+        GUI.DrawTexture(new Rect(0, 0, texture.width, texture.height), texture);
     }
 }
